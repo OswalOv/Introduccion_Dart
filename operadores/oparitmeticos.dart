72181f43d908fb3fa25fa53c61e92a8d415df484
OswalOv <oswaldovladimirduran@gmail.com>
2024-10-14 17:56:35 -0600

    Adding operator folder

---
+++ operadores/oparitmeticos.dart
@@ -0,0 +1,13 @@
+void main() {
+  int suma = 5 + 8 + 7;
+  double resta = 10 - 5 - 4.5;
+  int multip = 5 * 8;
+  double division = 8 / 5;
+  int modulo = 7 % 4;
+
+  print("Resta: $resta");
+  print("Suma: $suma");
+  print("Multiplicación: $multip");
+  print("División: $division");
+  print("Módulo: $modulo");
+}
